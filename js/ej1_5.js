f95bc8e30987f1bb243e227a0cfb1812ce59cf7b
alex <acosme1988@gmail.com>
2023-05-23 19:51:07 +0200

    ej1-6

---
+++ js/ej1_5.js
@@ -0,0 +1,26 @@
+// ej1
+let a=2
+let b=10
+let resultado=0
+resultado= a+b
+console.log(resultado)
+// ej2
+let radio=10
+resultado= Math.PI*radio**2
+console.log(resultado)
+// ej3
+let base=6
+let altura=10
+resultado= (base*altura)/2
+console.log(resultado)
+// ej4
+let temperaturaCelcius=24
+resultado=(temperaturaCelcius*9/5)+32
+console.log(resultado)
+// ej5
+let=precioProducto= 100
+let=porcentajeDescuento= 80;
+let porcentaje=0
+porcentaje=precioProducto -( precioProducto*porcentajeDescuento/100) 
+console.log(porcentaje)
+// ej6
---
+++ js/ej6.js
@@ -0,0 +1,8 @@
+let a = prompt('escribe numero')
+let b= a%2
+if (b==0) {
+    console.log('es par')
+}
+else {
+    console.log('es inpar')
+}
